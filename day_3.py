b60ace9608ce05c575d71d74fd590176180f323b
Daniel Caccavelli <42353038+dacaccavelli@users.noreply.github.com>
2025-03-17 10:15:54 -0400

    Day 3 part 2

--- day_3.py
+++ day_3.py
@@ -1,23 +1,31 @@
-
 import re
 
 
-pattern = re.compile(r"mul\(([0-9]+),([0-9]+)\)")
+do_dont_pattern = re.compile(r"do\(\).*?don't\(\)") # Non-Greedy Matching: The *? ensures that the pattern matches the smallest possible portion of the string between do() and don't().
+mul_pattern = re.compile(r"mul\((\d+),(\d+)\)")
 # pattern = re.compile("mul")
 
+file_content_str: str = ''
 with open('data/day_3.txt', 'r') as f:
-    file_content = f.read()
 
-# example = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+    file_content = f.readlines()
+    file_content_str = '\t'.join([line.strip() for line in file_content])
+    first = file_content_str.find('do()')
+    last = file_content_str.rfind("don't()") + 7
+    file_content_str = file_content_str[first:last]
 
-result = pattern.findall(file_content)
+do_dont_result = do_dont_pattern.findall(file_content_str)
+
 
 total = 0
-for match in result:
-    num1, num2 = match
-    num1 = int(num1)
-    num2 = int(num2)
-    total += num1 * num2
+for substring in do_dont_result:
+    result = mul_pattern.findall(substring)
+
+    for match in result:
+        num1, num2 = match
+        num1 = int(num1)
+        num2 = int(num2)
+        total += num1 * num2
 
 
 print(total)
